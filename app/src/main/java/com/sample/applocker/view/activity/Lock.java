1287ac2f6811fa027c8c6334ea93771ec96ecee3
SingleCycleKing <zhaoqiwang@hustunique.com>
2015-08-14 15:14:24 +0800

    commit

--- app/src/main/java/com/sample/applocker/view/activity/Lock.java
+++ app/src/main/java/com/sample/applocker/view/activity/Lock.java
@@ -5,19 +5,12 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.avazu.applocker.R;
-import com.chartboost.sdk.CBLocation;
-import com.chartboost.sdk.Chartboost;
-import com.chartboost.sdk.ChartboostDelegate;
-import com.chartboost.sdk.Libraries.CBLogging;
-import com.chartboost.sdk.Model.CBError;
 import com.sample.applocker.adapter.PagerAdapter;
 import com.sample.applocker.util.AppConstant;
 import com.sample.applocker.util.BasicUtil;
@@ -47,10 +40,7 @@ public class Lock extends BaseActivity {
 
     @Override
     protected void init() {
-        Chartboost.startWithAppId(this, "557fddf804b0162ea2b37449", "329bddc67a53e9ecdd6f398579eadfdc1d8dae74");
-        Chartboost.setLoggingLevel(CBLogging.Level.ALL);
-        Chartboost.setDelegate(delegate);
-        Chartboost.onCreate(this);
+
 
 
         appIcon.setVisibility(View.VISIBLE);
@@ -116,187 +106,4 @@ public class Lock extends BaseActivity {
         super.onWindowFocusChanged(hasFocus);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Chartboost.onStart(this);
-        Chartboost.showInterstitial(CBLocation.LOCATION_LEADERBOARD);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Chartboost.onResume(this);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Chartboost.onPause(this);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Chartboost.onStop(this);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Chartboost.onDestroy(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (Chartboost.onBackPressed())
-            return;
-        else
-            super.onBackPressed();
-    }
-
-    private ChartboostDelegate delegate = new ChartboostDelegate() {
-        String TAG = "fuck";
-
-        @Override
-        public boolean shouldRequestInterstitial(String location) {
-            Log.i(TAG, "SHOULD REQUEST INTERSTITIAL '" + (location != null ? location : "null"));
-            return true;
-        }
-
-        @Override
-        public boolean shouldDisplayInterstitial(String location) {
-            Log.i(TAG, "SHOULD DISPLAY INTERSTITIAL '" + (location != null ? location : "null"));
-            return true;
-        }
-
-        @Override
-        public void didCacheInterstitial(String location) {
-            Log.i(TAG, "DID CACHE INTERSTITIAL '" + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didFailToLoadInterstitial(String location, CBError.CBImpressionError error) {
-            Log.i(TAG, "DID FAIL TO LOAD INTERSTITIAL '" + (location != null ? location : "null") + " Error: " + error.name());
-            Toast.makeText(getApplicationContext(), "INTERSTITIAL '" + location + "' REQUEST FAILED - " + error.name(), Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void didDismissInterstitial(String location) {
-            Log.i(TAG, "DID DISMISS INTERSTITIAL: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didCloseInterstitial(String location) {
-            Log.i(TAG, "DID CLOSE INTERSTITIAL: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didClickInterstitial(String location) {
-            Log.i(TAG, "DID CLICK INTERSTITIAL: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didDisplayInterstitial(String location) {
-            Log.i(TAG, "DID DISPLAY INTERSTITIAL: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public boolean shouldRequestMoreApps(String location) {
-            Log.i(TAG, "SHOULD REQUEST MORE APPS: " + (location != null ? location : "null"));
-            return true;
-        }
-
-        @Override
-        public boolean shouldDisplayMoreApps(String location) {
-            Log.i(TAG, "SHOULD DISPLAY MORE APPS: " + (location != null ? location : "null"));
-            return true;
-        }
-
-        @Override
-        public void didFailToLoadMoreApps(String location, CBError.CBImpressionError error) {
-            Log.i(TAG, "DID FAIL TO LOAD MOREAPPS " + (location != null ? location : "null") + " Error: " + error.name());
-            Toast.makeText(getApplicationContext(), "MORE APPS REQUEST FAILED - " + error.name(), Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void didCacheMoreApps(String location) {
-            Log.i(TAG, "DID CACHE MORE APPS: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didDismissMoreApps(String location) {
-            Log.i(TAG, "DID DISMISS MORE APPS " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didCloseMoreApps(String location) {
-            Log.i(TAG, "DID CLOSE MORE APPS: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didClickMoreApps(String location) {
-            Log.i(TAG, "DID CLICK MORE APPS: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didDisplayMoreApps(String location) {
-            Log.i(TAG, "DID DISPLAY MORE APPS: " + (location != null ? location : "null"));
-        }
-
-        @Override
-        public void didFailToRecordClick(String uri, CBError.CBClickError error) {
-            Log.i(TAG, "DID FAILED TO RECORD CLICK " + (uri != null ? uri : "null") + ", error: " + error.name());
-            Toast.makeText(getApplicationContext(), "FAILED TO RECORD CLICK " + (uri != null ? uri : "null") + ", error: " + error.name(), Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public boolean shouldDisplayRewardedVideo(String location) {
-            Log.i(TAG, String.format("SHOULD DISPLAY REWARDED VIDEO: '%s'", (location != null ? location : "null")));
-            return true;
-        }
-
-        @Override
-        public void didCacheRewardedVideo(String location) {
-            Log.i(TAG, String.format("DID CACHE REWARDED VIDEO: '%s'", (location != null ? location : "null")));
-        }
-
-        @Override
-        public void didFailToLoadRewardedVideo(String location,
-                                               CBError.CBImpressionError error) {
-            Log.i(TAG, String.format("DID FAIL TO LOAD REWARDED VIDEO: '%s', Error:  %s", (location != null ? location : "null"), error.name()));
-            Toast.makeText(getApplicationContext(), String.format("DID FAIL TO LOAD REWARDED VIDEO '%s' because %s", location, error.name()), Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void didDismissRewardedVideo(String location) {
-            Log.i(TAG, String.format("DID DISMISS REWARDED VIDEO '%s'", (location != null ? location : "null")));
-        }
-
-        @Override
-        public void didCloseRewardedVideo(String location) {
-            Log.i(TAG, String.format("DID CLOSE REWARDED VIDEO '%s'", (location != null ? location : "null")));
-        }
-
-        @Override
-        public void didClickRewardedVideo(String location) {
-            Log.i(TAG, String.format("DID CLICK REWARDED VIDEO '%s'", (location != null ? location : "null")));
-        }
-
-        @Override
-        public void didCompleteRewardedVideo(String location, int reward) {
-            Log.i(TAG, String.format("DID COMPLETE REWARDED VIDEO '%s' FOR REWARD %d", (location != null ? location : "null"), reward));
-        }
-
-        @Override
-        public void didDisplayRewardedVideo(String location) {
-            Log.i(TAG, String.format("DID DISPLAY REWARDED VIDEO '%s' FOR REWARD", location));
-        }
-
-        @Override
-        public void willDisplayVideo(String location) {
-            Log.i(TAG, String.format("WILL DISPLAY VIDEO '%s", location));
-        }
-
-    };
 }
